7843a2ab66d8d041c8c1cbc68b1152c5905d5211
brunoferromega <brunoferromega@outlook.com>
2024-05-21 09:52:31 -0300

    feat(github_issues.ex): implement fetch of issues to the github

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -7,7 +7,7 @@ defmodule Issues.Cli do
   table of the last _n_ issues in a github project
   """
 
-  def run(argv), do: parse_args(argv)
+  def run(argv), do: argv |> parse_args |> process
 
   @doc """
   `argv` can be -h or --help which returns :help
@@ -27,4 +27,14 @@ defmodule Issues.Cli do
 
   defp args_to_internal_representation([user, project]), do: {user, project, @default_count}
   defp args_to_internal_representation(_), do: :help
+
+  def process(:help) do
+    IO.puts("""
+    usage: issues <user> <project> [count | #{@default_count}]
+    """)
+
+    System.halt(0)
+  end
+
+  def process({user, project, _count}), do: Issues.GithubIssues.fetch(user, project)
 end
---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,14 @@
+defmodule Issues.GithubIssues do
+  @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response()
+  end
+
+  def issues_url(user, project), do: "https://api.github.com/repos/#{user}/#{project}/issues"
+
+  def handle_response({:ok, %{status_code: 200, body: body}}), do: {:ok, body}
+  def handle_response({_, %{status_code: _, body: body}}), do: {:erro, body}
+end
