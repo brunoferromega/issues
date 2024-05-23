e2d40a5f8ecc29d43f2e2224637b7edc418b7d55
brunoferromega <brunoferromega@outlook.com>
2024-05-23 19:19:27 -0300

    feat(mix.exs): add markdown and ex doc

--- mix.exs
+++ mix.exs
@@ -5,7 +5,9 @@ defmodule Issues.MixProject do
     [
       app: :issues,
       escript: escript_config(),
-      version: "0.1.0",
+      version: "0.0.1",
+      name: "Issues",
+      source_url: "https://github.com/brunoferromega/issues",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -25,7 +27,9 @@ defmodule Issues.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:httpoison, "~> 2.2.1"},
-      {:poison, "~> 5.0.0"}
+      {:poison, "~> 5.0.0"},
+      {:earmark, "~> 1.4"},
+      {:ex_doc, "~> 0.33.0"}
     ]
   end
 
