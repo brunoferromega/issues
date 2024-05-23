f02e4e8fce05d32823575c6fbe71a1bc029868cb
brunoferromega <brunoferromega@outlook.com>
2024-05-23 19:08:30 -0300

    feat(config.exs/Issues.Cli): add logger

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,8 +1,11 @@
 defmodule Issues.GithubIssues do
+  require Logger
   @github_url Application.compile_env(:issues, :github_url)
   @user_agent [{"User-agent", "Elixir dave@pragprog.com"}]
 
   def fetch(user, project) do
+    Logger.info("Fetching #{user}'s project #{project}")
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response()
@@ -11,6 +14,9 @@ defmodule Issues.GithubIssues do
   def issues_url(user, project), do: "#{@github_url}/repos/#{user}/#{project}/issues"
 
   def handle_response({:ok, %{status_code: status_code, body: body}}) do
+    Logger.info("Got response: status code=#{status_code}")
+    Logger.debug(fn -> inspect(body) end)
+
     {
       status_code |> check_status(),
       body |> Poison.Parser.parse!()
